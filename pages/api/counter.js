88f406cafa34c7b60bb3086982a8965a029a0b7e
Joao Vidal <joao@joao.vidal>
2025-03-13 17:01:11 -0300

    Mudança do KV para Upstash

--- pages/api/counter.js
+++ pages/api/counter.js
@@ -1,16 +1,22 @@
-import { kv } from '@vercel/kv';
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        const count = await kv.get('vamo_dale') || 0;
-        return res.json({ count });
-    }
+  if (req.method === "GET") {
+    const count = await redis.get("vamo_dale") || 0;
+    return res.json({ count });
+  }
 
-    if (req.method === 'POST') {
-        const newCount = (await kv.get('vamo_dale') || 0) + 1;
-        await kv.set('vamo_dale', newCount);
-        return res.json({ count: newCount });
-    }
+  if (req.method === "POST") {
+    const current = (await redis.get("vamo_dale")) || 0;
+    const newCount = current + 1;
+    await redis.set("vamo_dale", newCount);
+    return res.json({ count: newCount });
+  }
 
-    res.status(405).end();
+  res.status(405).end();
 }
